ed8e2d1d333781874d5d5d6ab8dd412c61d502fc
switchlove <switchbladelove@gmail.com>
2017-05-30 14:45:58 -0500

    Update mdn.sh

--- mdn.sh
+++ mdn.sh
@@ -3,8 +3,7 @@
 # First run updates
 apt-get update && apt-get upgrade -y
 
-# Getting node and client files
-wget https://github.com/MysteriumNetwork/node/releases/download/0.0.6/mysterium-client_linux_amd64.deb
+# Getting node
 wget https://github.com/MysteriumNetwork/node/releases/download/0.0.6/mysterium-node_linux_amd64.deb
 
 # Prerequisites
@@ -16,9 +15,3 @@ apt-get install --fix-broken
 
 service mysterium-node restart
 service mysterium-node status
-
-dpkg --install mysterium-client_linux_amd64.deb
-apt-get install --fix-broken
-
-service mysterium-client restart
-service mysterium-client status
