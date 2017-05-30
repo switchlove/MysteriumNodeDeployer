feb778c3c528b926f6f9904b359d9851d44164a3
switchlove <switchbladelove@gmail.com>
2017-05-30 14:29:25 -0500

    Update mdn.sh

--- mdn.sh
+++ mdn.sh
@@ -1,14 +1,14 @@
 
 
 # First run updates
-apt-get -y update && apt-get -y upgrade
+apt-get update && apt-get upgrade -y
 
 # Getting node and client files
 wget https://github.com/MysteriumNetwork/node/releases/download/0.0.6/mysterium-client_linux_amd64.deb
 wget https://github.com/MysteriumNetwork/node/releases/download/0.0.6/mysterium-node_linux_amd64.deb
 
 # Prerequisites
-apt-get -y install openvpn
+apt-get install openvpn  -y
 
 # Install Node and Client
 dpkg --install mysterium-node_linux_amd64.deb
